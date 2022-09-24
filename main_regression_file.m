ed31bc190a0631ea42d01d9dac4869df6473d1f5
Martin <martin_reinke@gmx.de>
2022-09-24 12:45:13 +0200

    check: multiple models

---
+++ main_regression_file.m
@@ -0,0 +1,59 @@
+%% Example 1
+% Load default sample data from mathworks
+load carsmall
+X = [Weight,Horsepower,Acceleration];
+%Fit a linear regression model using fitlm.
+mdl = fitlm(X,MPG);
+
+% Function takes a linear Model as input and outputs a seperate .tex file
+tabPosition = 'H';
+tabCaption = 'My table names goes here'; 
+tabLabel = 'tab:myLabel';
+tabNote = 'This a long note explaining what I did in each model.';
+
+% Call mdl2latex and createx tex output for a single regression output
+modelName = '& Baseline Model 1';
+reg2latex({mdl},'myTable1.tex', tabPosition, tabCaption, tabLabel, modelName, tabNote);
+
+%% Example 2
+% Adding more regression models into one output table. Suppose you run multiple models and want to store the results column
+% wise.
+mdl1 = fitlm([Weight,Horsepower],MPG);
+mdl2 = fitlm(Weight,MPG);
+
+% Call mdl2latex and createx tex output if I would have used two models
+modelName = '& \textbf{Baseline Model} 1 & Another model 2';
+tabCaption = 'Please make my first model bold'; 
+reg2latex({mdl, mdl1},'myTable2.tex', tabPosition, tabCaption, tabLabel, modelName, tabNote);
+
+% Call mdl2latex and createx tex output for all my models
+modelName = '& \textbf{Baseline Model} 1 & Another model 2 & Another fancy model 3';
+reg2latex({mdl2, mdl, mdl1},'myTable3.tex', tabPosition, tabCaption, tabLabel, modelName, tabNote);
+
+%% Example 3
+% Suppose you have results from a custom regression output in a struct then mdl2latex works as well.
+% Consider using the ols function from the Spacial Econometric Toolbox from LeSage (Link:)
+olsStruct = ols(Acceleration, [Weight]);
+pValue = 2 * min(tcdf(olsStruct.tstat, olsStruct.nobs-1), 1-tcdf(olsStruct.tstat, olsStruct.nobs-1));
+
+% Note that the structure of your input has to follow this format
+myTable = table(olsStruct.beta, olsStruct.bstd, olsStruct.tstat, pValue,...
+                'VariableNames', {'Estimate', 'SE', 'tStat', 'pValue'}, 'RowNames', {'weight'});
+myMdl = struct;
+myMdl.CoefficientNames = myTable.Properties.RowNames';
+myMdl.Coefficients = myTable;
+myMdl.NumCoefficients = olsStruct.nvar;
+
+% Add fields to be added after coefficients in the same convention als in mdl
+addInfo.addFieldName = {'N', 'Rsquared', 'FirmFE'}; 
+
+addInfo.model1.(addInfo.addFieldName{1}) = olsStruct.nobs;
+addInfo.model1.(addInfo.addFieldName{2}) = olsStruct.rsqr;
+addInfo.model1.(addInfo.addFieldName{3}) = 'Yes';
+
+addInfo.model2.(addInfo.addFieldName{1}) = mdl.NumObservations;
+addInfo.model2.(addInfo.addFieldName{2}) = mdl.Rsquared.Ordinary;
+addInfo.model2.(addInfo.addFieldName{3}) = 'No';
+
+modelName = '& \textbf{Baseline Model} 1 & Test';
+reg2latex({mdl, myMdl},'myCustomTable.tex', tabPosition, tabCaption, tabLabel, modelName, tabNote, addInfo)
---
+++ reg2latex.m
@@ -0,0 +1,225 @@
+function reg2latex(mdl, varargin)
+% function mdl2latex(mdl, varargin)
+% Produces a tex document summarizing the regression output.
+%
+% Syntax:
+%	- sum_stat = mdl2latex(mdl,filename, tabPosition, tabCaption, modelName, tabLabel, tabNote, addInfo)
+%
+% Note: 
+%   - The function prints out the standard errors below the coefficients in
+%     brackets.
+%   - The function can handel the input of multiple models in order to
+%     produce a table which list the results column wise.
+%   - Numbers are rounded to the 4th digit.
+%
+% Input:
+%   Required:
+%	- mdl: 1x1 linear model (required)
+%   Optional:
+%   - filename: char (e.g.'myFolder/myTable1.tex')
+%   - tabPosition: char (e.g. 'H' or other latex specific position settings)
+%   - tabCaption: char (e.g. 'My personal caption')
+%   - modelName: char (e.g. 'My fancy model 1')
+%   - tabLabel: char (e.g. 'tab:myLabel')
+%   - tabNote: char ('Some customized text.')
+%   - addInfo: struct with additional information to be added below the
+%   coefficients in the output table.
+%
+% Output:
+%	- .tex: file to the current folder or the path specified in the
+%	optional variable filename.
+
+% Determine number of models
+numModel = size(mdl,2);
+% Get number of column separator for latex and the number of necessary columns in table
+colSep = repmat('&',1,numModel);
+colPos = repmat('c',1,numModel);
+
+% Define default settings for table output
+defaultFilename = 'table.tex';
+defaultTabPosition = 'H';
+defaultTabCaption = '';
+defaultTabLabel = '';
+defaultTabNote = '';
+defaultTabInfo = struct;
+
+% Construct default model names if no specific model name is parsed into the function
+defaultModelName = '';
+for iCol = 1:numModel
+    temp = append(string(colSep(iCol)),' Model ',string(iCol));
+    defaultModelName = char(append(defaultModelName, temp));
+    clear temp
+end
+
+p = inputParser;
+addRequired(p, 'mdl');
+addOptional(p, 'filename', defaultFilename, @ischar);
+addOptional(p, 'tabPosition', defaultTabPosition, @ischar);
+addOptional(p, 'tabCaption', defaultTabCaption, @ischar);
+addOptional(p, 'tabLabel', defaultTabLabel, @ischar);
+addOptional(p, 'modelName', defaultModelName, @ischar)
+addOptional(p, 'tabNote', defaultTabNote, @ischar);
+addOptional(p, 'addInfo', defaultTabInfo, @isstruct);
+parse(p, mdl, varargin{:});
+
+
+% Check wether the modelName is correctly specified. If not set default
+% model Name again
+if count(p.Results.modelName,'&') ~= numModel   
+    modelName = defaultModelName;
+    warning('The model names have been incorrectly specified. They are replaced by the default names.')
+else
+    modelName = p.Results.modelName;
+end
+
+% Open file
+file = fopen(p.Results.filename, 'w');
+
+% Start table
+fprintf(file, '\\begin{table}[%s] \n', p.Results.tabPosition);
+fprintf(file, '\\centering \n');
+% Set caption if input is not empty, else do not set caption
+if ~isempty(p.Results.tabCaption)
+    fprintf(file, '\\caption{%s} \n', p.Results.tabCaption);
+end
+fprintf(file, '\\label{%s}\n', p.Results.tabLabel);
+fprintf(file, '\\begin{threeparttable}\n');
+fprintf(file, '\\begin{tabular}{l%s}\n',colPos);
+fprintf(file, '\\toprule \n');
+% Design header
+fprintf(file, '%s \\\\ \n', modelName);
+fprintf(file, '\\midrule \n');
+
+% Get maximum number of explanatory variables and unique number of
+% variables
+coefficients = nan(numModel,1);
+
+for iModel = 1:numModel
+    coefficients(iModel) = p.Results.mdl{1,iModel}.NumCoefficients;
+end
+
+% Get unique coefficient names
+coefficientsNames = cell(1,numModel);
+for iModel = 1:numModel
+    coefficientsNamesTemp = char(p.Results.mdl{1,iModel}.CoefficientNames');
+    coefficientsNames{1,iModel} =  char(coefficientsNamesTemp);
+end
+
+% Append vertically coefficient names
+coefficientsNames = char(coefficientsNames);
+
+% Add cell array of unique coefficient names
+rowNamesUnique = unique(string(coefficientsNames));
+
+% Remove empty first row
+
+% Loop over models and append to one cell array
+temp = cell(max(coefficients*2),numModel+1); % Create empty cell array
+
+% Add unique row names to temp
+idxTemp = 1:2:size(temp,1);
+for iVar = 1:size(idxTemp,2)
+    temp(idxTemp(iVar),1) = {strtrim(char((rowNamesUnique(iVar))))};
+end
+
+for iModel=1:numModel
+    
+    outCell = createTabelCell(p.Results.mdl{iModel});
+    
+    % Add variables to cell
+    for iVar = 1:size(outCell,1)
+        % Find variable in cell
+        idxVar = find(strcmp(temp(:,1),outCell(iVar,1)));
+        if isempty(idxVar)
+            continue
+        else
+            temp(idxVar,iModel+1) = outCell(iVar,2); % add coefficients
+            temp(idxVar+1,iModel+1) = outCell(iVar+1,2); % add coefficients
+        end
+    end
+end
+
+% Loop through every row
+for iRow = 1:size(temp,1)
+    
+    % Check if Row number is divisible by 2. To place multirow
+    % accordingly
+    out=~rem(iRow,2)*iRow/2;
+    
+    % Format first column
+    if out == 0
+        % Format first column of variable names
+        textVar = append('\multirow{2}{*}{',cell2mat(temp(iRow,1)),'}&');
+        
+        % Call function and create column text
+        colText = createColText(temp,iRow);
+        % Construct full column text
+        colTextFull = append(textVar,colText);
+        % Write column text to file
+        fprintf(file,'%s \\\\ \n', colTextFull);
+        
+    else
+        
+        % Call function and create column text
+        colTextFull = createColText(temp,iRow);
+        
+        % Write column text to file. Added & to reflect the fact that first
+        % column is empty
+        fprintf(file,'& %s \\\\ \n', colTextFull);
+        
+    end
+end
+
+% Loop over the additional input into the models if additional info was
+% parsed
+if ~isempty(fieldnames(p.Results.addInfo))
+   
+    % Add additional regresssion statistics to the output table
+    fprintf(file, '\\midrule \n');
+    
+    for iAddInfo = 1:size(p.Results.addInfo.addFieldName,2)
+        
+        % Loop over additional entries
+        infoName = p.Results.addInfo.addFieldName{iAddInfo};
+
+        % Loop over models
+        tempModels = fieldnames(p.Results.addInfo);
+        
+        temp = cell(1,numModel+1);
+        
+        % Write variable name into cell array
+        temp{1,1} = infoName;
+        
+        % Loop starts at 2 because the first input in the struct contains the
+        % names of the additional input
+        for iModel = 2:size(tempModels,1)
+
+          % Add information per model to cell
+          temp{1,iModel} = p.Results.addInfo.(tempModels{iModel}).(infoName);
+            
+        end
+
+        % Call function and create column text
+        colText = createColText(temp,1);
+        % Construct full column text
+        colTextFull = append(infoName,' & ',colText);
+        
+        % Print row to file
+        fprintf(file,'%s \\\\ \n', colTextFull);
+
+    end
+    
+end
+
+% Closing the file
+fprintf(file, '\\bottomrule \n');
+fprintf(file, '\\end{tabular} \n');
+fprintf(file, '\\begin{tablenotes} \n');
+fprintf(file, '\\scriptsize \n');
+fprintf(file, '\\item \\leavevmode\\kern-\\scriptspace\\kern-\\labelsep Note:$^{*}$p$<$0.10,$^{**}$p$<$0.05,$^{***}$p$<$0.01 %s \n', p.Results.tabNote);
+fprintf(file, '\\end{tablenotes}\n');
+fprintf(file,'\\end{threeparttable} \n');
+fprintf(file, '\\end{table} \n ');
+fclose(file);
+
+end
