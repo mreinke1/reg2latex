cbba04a23779ee9d17f3b010f8ab35d73a7a189d
Martin Reinke <martin_reinke@gmx.de>
2023-04-26 18:16:46 +0200

    fix: function call

--- main_regression_file.m
+++ main_regression_file.m
@@ -13,7 +13,7 @@ tabNote = 'This a long note explaining what I did in each model.';
 
 % Call mdl2latex and createx tex output for a single regression output
 modelName = '& Baseline Model 1';
-reg2latex({mdl},'myTable1.tex', tabPosition, tabCaption, tabLabel, modelName, tabNote);
+reg2latex({mdl},'filename','myTable1.tex', 'tabPosition',tabPosition, 'tabCaption',tabCaption, 'tabLabel',tabLabel, 'modelName', modelName, 'tabNote',tabNote);
 
 %% Example 2
 % Adding more regression models into one output table. Suppose you run multiple models and want to store the results column
@@ -24,11 +24,11 @@ mdl2 = fitlm(Weight,MPG);
 % Call mdl2latex and createx tex output if I would have used two models
 modelName = '& \textbf{Baseline Model} 1 & Another model 2';
 tabCaption = 'Please make my first model bold'; 
-reg2latex({mdl, mdl1},'myTable2.tex', tabPosition, tabCaption, tabLabel, modelName, tabNote);
+reg2latex({mdl, mdl1},'filename','myTable2.tex', 'tabPosition',tabPosition, 'tabCaption',tabCaption, 'tabLabel',tabLabel, 'modelName', modelName, 'tabNote',tabNote);
 
 % Call mdl2latex and createx tex output for all my models
 modelName = '& \textbf{Baseline Model} 1 & Another model 2 & Another fancy model 3';
-reg2latex({mdl2, mdl, mdl1},'myTable3.tex', tabPosition, tabCaption, tabLabel, modelName, tabNote);
+reg2latex({mdl2, mdl, mdl1},'filename','myTable3.tex', 'tabPosition',tabPosition, 'tabCaption',tabCaption, 'tabLabel',tabLabel, 'modelName', modelName, 'tabNote',tabNote);
 
 %% Example 3
 % Suppose you have results from a custom regression output in a struct then mdl2latex works as well.
@@ -56,4 +56,4 @@ addInfo.model2.(addInfo.addFieldName{2}) = mdl.Rsquared.Ordinary;
 addInfo.model2.(addInfo.addFieldName{3}) = 'No';
 
 modelName = '& \textbf{Baseline Model} 1 & Test';
-reg2latex({mdl, myMdl},'myCustomTable.tex', tabPosition, tabCaption, tabLabel, modelName, tabNote, addInfo)
+reg2latex({mdl, myMdl},'filename','myCustomTable.tex', 'tabPosition', tabPosition, 'tabCaption', tabCaption, 'tabLabel',tabLabel, 'modelName', modelName, 'tabNote',tabNote);
